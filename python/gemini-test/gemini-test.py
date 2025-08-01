0bd27015d529a482be7849b562d541dfcf025fb4
Rob Myers <me.robmyers@gmail.com>
2025-08-01 12:00:23 +0100

    feat: gemini-test connects to gemini and prints response

--- python/gemini-test/gemini-test.py
+++ python/gemini-test/gemini-test.py
@@ -1,4 +1,7 @@
 # pip3 install google-genai
+#
+# cd python/gemini-test
+# python3 gemini-test.py
 
 import os
 
@@ -8,3 +11,10 @@ gemini_api_key = os.popen('cat ~/.gemini_api_key').read()
 os.environ['GEMINI_API_KEY'] = gemini_api_key
 
 client = genai.Client()
+
+response = client.models.generate_content(
+  model='gemini-2.5-flash',
+  contents='Explain how AI works in a few words, please'
+)
+
+print(response.text)
