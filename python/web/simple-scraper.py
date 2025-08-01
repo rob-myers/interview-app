17121d0eeb68b68c98c24fe47cd1e974f66c4b3f
Rob Myers <me.robmyers@gmail.com>
2025-08-01 13:39:05 +0100

    feat: simple-scraper gets html and parses

--- python/web/simple-scraper.py
+++ python/web/simple-scraper.py
@@ -1,7 +1,16 @@
+# pip3 install requests
 import requests
+# from xml.dom import minidom
+# pip3 install beautifulsoup4
+from bs4 import BeautifulSoup
 
 search_term='foo'
 
 res = requests.get(f'https://www.google.com/search?q={search_term}')
 
-print(res.text)
+# print(res.text)
+# parsed = minidom.parseString('<foo></foo>')
+# parsed = minidom.parseString(res.text) # ❌
+
+parsed = BeautifulSoup(res.text, 'html.parser')
+print(parsed.prettify())
