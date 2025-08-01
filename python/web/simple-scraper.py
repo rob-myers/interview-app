5134dd81793dc846b0d14bfd4eccbe5e6a63e00b
Rob Myers <me.robmyers@gmail.com>
2025-08-01 13:56:14 +0100

    feat: simple-scraper gets a.hrefs

--- python/web/simple-scraper.py
+++ python/web/simple-scraper.py
@@ -1,16 +1,27 @@
+# pyright: reportUnknownMemberType=false
+# pyright: reportUnknownArgumentType=false
+# pyright: reportAttributeAccessIssue=false
+
 # pip3 install requests
 import requests
 # from xml.dom import minidom
 # pip3 install beautifulsoup4
 from bs4 import BeautifulSoup
 
-search_term='foo'
+search_term='bar+baz'
 
-res = requests.get(f'https://www.google.com/search?q={search_term}')
+# res = requests.get(f'https://www.google.com/search?q={search_term}')
+res = requests.get(f'http://localhost:3000/blog/about')
 
 # print(res.text)
 # parsed = minidom.parseString('<foo></foo>')
 # parsed = minidom.parseString(res.text) # ❌
 
 parsed = BeautifulSoup(res.text, 'html.parser')
-print(parsed.prettify())
+# print(parsed.prettify())
+
+
+anchors = parsed.find_all('a')
+
+for anchor in anchors:
+  print(anchor.get('href'))
