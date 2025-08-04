ea296e2b04846abd9736bce82a1b4c29e07f56eb
Rob Myers <me.robmyers@gmail.com>
2025-08-04 10:57:09 +0100

    feat: extend py cheatsheet examples

--- python/cheatsheet/cheatsheet.py
+++ python/cheatsheet/cheatsheet.py
@@ -3,10 +3,8 @@
 pows_of_two = list(map(lambda x: 2 ** x, range(10)))
 print('pows_of_two', pows_of_two)
 
-third_power = pows_of_two[3]
-print('third_power', third_power)
-odd_pows_of_two = pows_of_two[1 : -1 : 2]
-print('odd_pows_of_two', odd_pows_of_two)
+print('third_power', pows_of_two[3])
+print('odd_pows_of_two', pows_of_two[1 : -1 : 2])
 
 pows_of_two += [1024] # OR pows_of_two.append(1024)
 pows_of_two += [2048, 4096]  # OR pows_of_two.extend([2048, 4096])
@@ -32,12 +30,38 @@ print(f'largest/least random ({random_tries} tries)', sup_random_tries, inf_rand
 print(f'sum of [0, 1]-valued random variables ({random_tries})', sum_random_tries)
 print(f'product of [1, 2]-valued random variables ({random_tries})', mul_random_tries)
 
-diagonal_relation = [[x,x] for x in range(10) ]
-print('diagonal_relation', diagonal_relation)
+import itertools
+print('elementwise_sum', [sum(x) for x in zip(range(5), range(5))])
+print('diagonal_relation', [[x,x] for x in range(10) ])
 # mixture of positional and named args
 sorted_by_second = sorted([[x, 10 - x] for x in range(10)], key=lambda x: x[1])
 sorted_by_both = sorted([[4, 1], [7, 1], [7, 0], [49, 1]], key=lambda x: [x[0], x[1]])
 print('sorted_by_second', sorted_by_second, '\nsorted_by_both', sorted_by_both)
+print('flattened_list', list(itertools.chain.from_iterable([[1, 2], [3, 4]])))
+
+print(
+  f'length_of_list: {len(list("abc"))}',
+  f'length_of_string {len("abc")}',
+  f'number_of_occurrences (a) {[1, 3, 3, 1].count(3)}',
+  f'number_of_occurrences (b) {"the quick brown fox jumped over the lazy dog".count("o")}',
+  f'number_of_occurrences (c) {"the quick brown fox jumped over the lazy dog".count("the")}',
+  f'number_of_occurrences (d) {[[1,2],[1,2]].count([1,2])}',
+  f'number_of_occurrences (d) {[[1,2],[1,2]].count([1,2])}',
+  f'index_of_el (a) {[1,2,3].index(3)}',
+  f'index_of_el (a) {"abcefghijk".index("fg")}',
+  sep='\n'
+)
+els = list(range(5))
+print(f'el_popped: {els.pop()}')
+some_random_numbers = list(map(lambda x: random.random(), range(6)))
+some_random_numbers.insert(3, 42)
+print('inserted_42_in_middle', some_random_numbers)
+some_random_numbers.remove(42)
+print('removed_42_from_list', some_random_numbers)
+some_random_numbers.clear()
+print('cleared_list', some_random_numbers)
+
+# Dictionary 🚧
 
 ###
 
